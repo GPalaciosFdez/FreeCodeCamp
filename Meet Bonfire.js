81e843d03506491b88fa9622b75032791230e728
GPalaciosFdez <g.palaciosfdez@gmail.com>
2015-06-29 20:50:16 +0200

    Delete Meet Bonfire.js

--- Meet Bonfire.js
+++
@@ -1,8 +0,0 @@
-function meetBonfire(argument) {
-  // Good luck!
-  console.log("you can read this function's argument in the developer tools", argument);
-
-  return true;
-}
-
-meetBonfire("You can do this!");
