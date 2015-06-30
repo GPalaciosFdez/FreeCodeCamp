9360ff7584ecfa0f2af5cbc4e67b55a2b9eb74f9
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-06-30 15:30:19 +0200

    Repeat a String Repeat a String

---
+++ RepeatAStringRepeatAString.js
@@ -0,0 +1,15 @@
+function repeat(str, num) {
+  // repeat after me
+  if(num<=0){
+    return "";
+  }
+  else{
+    var s="";
+    for(var i=1; i<=num; i++){
+      s=s+str;
+    }
+    return s;
+  }
+}
+
+repeat('abc', 3);
