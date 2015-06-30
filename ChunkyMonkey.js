4b15b01fcb9d4a9551bf04f812f0c703e05c0203
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-06-30 15:32:31 +0200

    Chunky Monkey

---
+++ ChunkyMonkey.js
@@ -0,0 +1,14 @@
+function chunk(arr, size) {
+  // Break it up.
+  arr_final=[];
+  len=arr.length;
+  var i=0;
+  while(i<len/size){
+    arr_final.push(arr.slice(0,size));
+    arr=arr.slice(size);
+    i++;
+  }
+  return arr_final;
+}
+
+chunk(['a', 'b', 'c', 'd'], 2);
