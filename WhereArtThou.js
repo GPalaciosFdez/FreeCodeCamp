35971fa7a3091eed708c6f49c4e745809171b5e3
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:14:54 +0200

    Where art thou

---
+++ WhereArtThou.js
@@ -0,0 +1,12 @@
+function where(collection, source) {
+  var arr = [];
+  // What's in a name?
+  for (var i in collection){
+    if (collection[i].last===source.last){
+      arr.push(collection[i]);
+    }
+  }
+  return arr;
+}
+
+where([{ first: 'Romeo', last: 'Montague' }, { first: 'Mercutio', last: null }, { first: 'Tybalt', last: 'Capulet' }], { last: 'Capulet' });
