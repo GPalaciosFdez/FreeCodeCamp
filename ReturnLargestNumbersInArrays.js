d54b0252ea638ae0be0d9bee9028a6ef7d0fea6a
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:30:05 +0200

    Return Largest Numbers in Arrays

--- ReturnLargestNumbersInArrays.js
+++ ReturnLargestNumbersInArrays.js
@@ -1,17 +1,17 @@
 function largestOfFour(arr) {
-  // You can do this!
-  largest=[];
-  
-  for(var i=0; i<arr.length; i++){
-    num=arr[i][0];
-    for(var j=0; j<arr[i].length; j++){
-      if(arr[i][j]>num){
-        num=arr[i][j];
-      }
+    // You can do this!
+    largest = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        num = arr[i][0];
+        for (var j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > num) {
+                num = arr[i][j];
+            }
+        }
+        largest.push(num);
     }
-    largest.push(num);
-  }
-  return largest;
+    return largest;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
