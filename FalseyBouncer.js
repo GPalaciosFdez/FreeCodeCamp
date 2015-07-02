e5c4d22931a7c5e5130291264a90ed93e3858a7d
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:14:42 +0200

    Falsey Bouncer

---
+++ FalseyBouncer.js
@@ -0,0 +1,14 @@
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  arr=arr.filter(function(falsey){
+    if(Boolean(falsey)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  });
+  return arr;
+}
+
+bouncer([7, 'ate', '', false, 9]);
