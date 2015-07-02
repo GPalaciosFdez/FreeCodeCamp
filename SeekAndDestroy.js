a76c33bef2f04d945c35519708e8e9d512bb282f
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:15:56 +0200

    Seek and Destroy

---
+++ SeekAndDestroy.js
@@ -0,0 +1,13 @@
+function destroyer(arr) {
+  // Remove all the values
+  for (var i=0; i<arr.length; i++){
+    for (var j=0; j<arguments.length; j++){
+      if(arr[i]===arguments[j]){
+        arr.splice(i,1);
+      }
+    }
+  }
+  return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
