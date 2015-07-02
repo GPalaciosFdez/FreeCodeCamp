04aa70464bcfdcf5b43940b0229e0b94ff1607df
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:31:26 +0200

    Title Case a Sentence

--- TitleCaseASentence.js
+++ TitleCaseASentence.js
@@ -1,13 +1,13 @@
 function titleCase(str) {
-  str=str.split(" ");
-  
-  for(var i=0; i<str.length; i++){
-    str[i]=str[i].toLowerCase();
-    str[i]=str[i].charAt(0).toUpperCase()+str[i].substring(1);
-  }
-  
-  str=str.join(" ");
-  return str;
+    str = str.split(" ");
+
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].toLowerCase();
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].substring(1);
+    }
+
+    str = str.join(" ");
+    return str;
 }
 
 titleCase("I'm a little tea pot");
