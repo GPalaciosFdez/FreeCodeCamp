f3a485d56ae76fb2cfb41006821613ae8b4e466f
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-02 23:29:04 +0200

    Mutations

--- Mutations.js
+++ Mutations.js
@@ -1,12 +1,12 @@
 function mutation(arr) {
-  arr[0]=arr[0].toLowerCase().split("");
-  arr[1]=arr[1].toLowerCase().split("");
-  for(var i=0; i<arr[1].length; i++){
-    if(arr[0].indexOf(arr[1][i])===-1){
-      return false;
+    arr[0] = arr[0].toLowerCase().split("");
+    arr[1] = arr[1].toLowerCase().split("");
+    for (var i = 0; i < arr[1].length; i++) {
+        if (arr[0].indexOf(arr[1][i]) === -1) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 mutation(['hello', 'hey']);
