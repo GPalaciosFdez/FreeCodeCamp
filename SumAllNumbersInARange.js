c314ee25fa0dcd5cc7e2d09e6cf1dee18bd4e9fb
Guillermo Palacios Fernández <g.palaciosfdez@gmail.com>
2015-07-04 00:11:57 +0200

    Sum All Numbers in a Range

--- SumAllNumbersInARange.js
+++ SumAllNumbersInARange.js
@@ -1,14 +1,7 @@
 function sumAll(arr) {
     var sum = 0;
-    var low = 0;
-    var high = 0;
-    if (arr[0] > arr[1]) {
-        low = arr[1];
-        high = arr[0];
-    } else {
-        low = arr[0];
-        high = arr[1];
-    }
+    var low = Math.min(arr[0], arr[1]);
+    var high = Math.max(arr[0], arr[1]);
 
     for (var i = low; i <= high; i++) {
         sum += i;
